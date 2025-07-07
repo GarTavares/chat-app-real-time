67b6f4e074adab226fcd4c387d39801f354e5906
Gabriel <biel.tavaresmoraes@gmail.com>
2025-07-07 00:11:18 -0300

    mensagem que resume a alteração

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,9 @@
-const socket = io("https://meuchat-api.onrender.com");
+//const socket = io("https://meuchat-api.onrender.com");
+const socket = io("http://localhost:3000");
+
+socket.on("connect", ()=> {
+  console.log("Socket conectado com ID:", socket.id);
+});
 
 function enviar() {
   const texto = document.getElementById("mensagem").value;
