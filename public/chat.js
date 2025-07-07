a7ad8dda0da1c0bcf76e7733725514f8fc8bfea8
Gabriel <biel.tavaresmoraes@gmail.com>
2025-07-07 00:13:05 -0300

    mensagem que resume a alteração

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
-//const socket = io("https://meuchat-api.onrender.com");
-const socket = io("http://localhost:3000");
+const socket = io("https://meuchat-api.onrender.com");
+//const socket = io("http://localhost:3000");
 
 socket.on("connect", ()=> {
   console.log("Socket conectado com ID:", socket.id);
